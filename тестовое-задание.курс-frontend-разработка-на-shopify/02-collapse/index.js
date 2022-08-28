fe734383439c36c5bf4e31aee4524e1dc317b46d
KauhareniaNastassia <kavgorenya_a@tut.by>
2022-08-29 00:09:35 +0300

    add task 2

--- тестовое-задание.курс-frontend-разработка-на-shopify/02-collapse/index.js
+++ тестовое-задание.курс-frontend-разработка-на-shopify/02-collapse/index.js
@@ -1,1 +1,36 @@
 /* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+
+let collapsibleContent = document.querySelector(".collapsible__content")
+
+let actionHideAndShow = document.querySelector(".collapsible__button")
+
+function hideOnClick() {
+    collapsibleContent.animate(
+        [
+            { opacity: 1},
+            { opacity: 0},
+        ],
+        {
+            duration: 3000,
+            fill: 'forwards'
+        }
+    )
+}
+
+function showOnClick() {
+    collapsibleContent.animate(
+        [
+            { opacity: 0},
+            { opacity: 1},
+        ],
+        {
+            duration: 3000,
+            fill: 'forwards'
+        }
+    )
+}
+
+actionHideAndShow.addEventListener('click', hideOnClick)
+actionHideAndShow.addEventListener('click', showOnClick)
+
+
