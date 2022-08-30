c856c231301b0e4398ce412d5807ecb8d5bb59e8
KauhareniaNastassia <kavgorenya_a@tut.by>
2022-08-30 23:55:08 +0300

    refactoring

--- тестовое-задание.курс-frontend-разработка-на-shopify/02-collapse/index.js
+++ тестовое-задание.курс-frontend-разработка-на-shopify/02-collapse/index.js
@@ -1,33 +1,59 @@
-
 let collapsibleContent = document.querySelector(".collapsible__content")
-
 let actionHideAndShow = document.querySelector(".collapsible__button")
+let visible = document.querySelector(".collapsible__action--visible")
+let hidden = document.querySelector(".collapsible__action--hidden")
 
 
 let hide = '';
-actionHideAndShow.addEventListener("click",function(){
-    if(hide) {
-        hide = collapsibleContent.animate(
-            [
-                { opacity: 0},
-                { opacity: 1},
-            ],
-            {
-                duration: 2000,
-                fill: 'forwards'
-            }
-        );
-        hide= ''
+
+let options = {
+    duration: 0,
+    fill: 'forwards'
+};
+
+let showBlock = [
+    {opacity: 0},
+    {opacity: 1},
+];
+
+let hideBlock = [
+    {opacity: 1},
+    {opacity: 0},
+];
+
+
+actionHideAndShow.addEventListener("click", function () {
+    if (hide) {
+        hide = [
+            collapsibleContent.animate(
+                showBlock,
+                options
+            ),
+            visible.animate(
+                showBlock,
+                options
+            ),
+            hidden.animate(
+                hideBlock,
+                options
+            )
+        ];
+        hide = ''
     } else {
-        hide = collapsibleContent.animate(
-            [
-                { opacity: 1},
-                { opacity: 0},
-            ],
-            {
-                duration: 2000,
-                fill: 'forwards'
-            }
-        )
+        hide = [
+            collapsibleContent.animate(
+                hideBlock,
+                options
+            ),
+            visible.animate(
+                hideBlock,
+                options
+            ),
+            hidden.animate(
+                showBlock,
+                options
+            )
+        ]
     }
 })
+
